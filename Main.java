7051f04fe390a644fd38f6f934929ab322ad6019
Kairi Noguchi <k.noguchi.sys21@morijyobi.ac.jp>
2022-03-17 12:25:08 +0900

    BMIを2回出力

--- Main.java
+++ Main.java
@@ -3,11 +3,13 @@ import java.util.Scanner;
 public class Main {
   public static void main(String[] args) {
     Scanner stdIn = new Scanner(System.in);
-
+    int i=0;
     double height = stdIn.nextDouble();
     double weight = stdIn.nextDouble();
+    while( i <= 1 ) {
     double bmi = weight / Math.pow(height, 2);
     System.out.println( bmi );
-
+    i = i + 1;
+    }
   }
 }
